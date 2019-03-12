6c330dbd7a40a8b0aae424777eb679202a43a936
Enrico Monte <enricomonte@MacBook-Pro-di-Enrico.local>
2019-03-12 23:15:43 +0100

    mod

--- app/src/main/java/it/univaq/mobileprogramming/myweather/MainActivity.java
+++ app/src/main/java/it/univaq/mobileprogramming/myweather/MainActivity.java
@@ -1,8 +1,12 @@
 package it.univaq.mobileprogramming.myweather;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -21,32 +25,50 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView t2_city,t4_date,t3_wind,t5_humidity;
+    Toolbar toolbar;
+    //private Snackbar snackbar;
+    //private View view;
+
+    private TextView t2_city,t4_date;
     private CircleView t1_temp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.weather_activity);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolBar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //view = findViewById(R.id.view);
 
         t1_temp = (CircleView)findViewById(R.id.weather_result);
         t2_city = (TextView)findViewById(R.id.city_country);
         t4_date = (TextView)findViewById(R.id.current_date);
-        t3_wind = (TextView)findViewById(R.id.wind_result);
-        t5_humidity = (TextView)findViewById(R.id.humidity_result);
 
-        find_weather();
+        try {
+            find_weather();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void find_weather() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    public void find_weather() throws JSONException {
         AsyncHttpClient client = new AsyncHttpClient();
-        client.get("https://api.openweathermap.org/data/2.5/weather?q=Lucera,it&appid=73f45256d96f6980fc804cca915873ea&units=metric&lang=it", new AsyncHttpResponseHandler() {
+        client.get("https://api.openweathermap.org/data/2.5/weather?q=Torrebruna,it&appid=73f45256d96f6980fc804cca915873ea&units=metric&lang=it", new AsyncHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody) {
                 // called when response HTTP status is "200 OK"
                 String in = new String(responseBody);
-                Log.d("Enri", in);
                 String response = new String(responseBody);
                 try {
                     parseMethod(response);
@@ -58,6 +80,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, byte[] responseBody, Throwable error) {
                 // called when response HTTP status is "404 error"
+                /*snackbar = Snackbar.make(view, "errore", snackbar.LENGTH_INDEFINITE);
+                snackbar.setDuration(3000);
+                snackbar.setAction("RIMUOVI", new View.OnClickListener(){
+                    @Override
+                    public void onClick(View v) {
+                        snackbar.dismiss();
+                    }
+                });
+                snackbar.show();*/
                 Toast.makeText(getApplicationContext(), "Errore: Inserire Città valida!", Toast.LENGTH_SHORT).show();
             }
 
@@ -69,22 +100,17 @@ public class MainActivity extends AppCompatActivity {
 
                     JSONObject main_object = jor.getJSONObject("main");
                     JSONObject country_object = jor.getJSONObject("sys");
-                    JSONObject wind_object = jor.getJSONObject("wind");
                     JSONArray array = jor.getJSONArray("weather");
                     JSONObject object = array.getJSONObject(0);
                     Double temp= main_object.getDouble("temp");
                     String country= String.valueOf(country_object.getString("country"));
                     String description = object.getString("description");
                     String city = jor.getString("name");
-                    String humidity= String.valueOf(main_object.getInt("humidity"));
-                    String wind= String.valueOf(wind_object.getDouble("speed"));
 
                     String temperatura = String.format ("%.1f", temp);
                     t1_temp.setTitleText(temperatura);
                     t2_city.setText(city + ", " + country);
                     t1_temp.setSubtitleText(description);
-                    t5_humidity.setText(humidity + "%");
-                    t3_wind.setText(wind + "km/h");
 
                     Date data = new  Date();
                     Locale.setDefault(Locale.ITALIAN);
